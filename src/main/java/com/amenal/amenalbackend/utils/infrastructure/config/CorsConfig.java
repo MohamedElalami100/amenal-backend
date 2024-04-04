aa4593a4f4aac8ac02271e176b3aa6715e257118
simo1111 <elalamimohamed2k17@gmail.com>
2024-04-04 23:25:56 +0000

    fix chart data bug

--- src/main/java/com/amenal/amenalbackend/utils/infrastructure/config/CorsConfig.java
+++ src/main/java/com/amenal/amenalbackend/utils/infrastructure/config/CorsConfig.java
@@ -1,14 +1,21 @@
 package com.amenal.amenalbackend.utils.infrastructure.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-	
-	@Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*"); // Allow requests from all origins
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
