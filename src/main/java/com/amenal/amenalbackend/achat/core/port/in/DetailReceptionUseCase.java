8761cb02b7e1c34aa7b0ca5666c72298d0f1ac41
simo1111 <elalamimohamed2k17@gmail.com>
2024-06-03 01:21:01 +0100

    implement reception

--- src/main/java/com/amenal/amenalbackend/achat/core/port/in/DetailReceptionUseCase.java
+++ src/main/java/com/amenal/amenalbackend/achat/core/port/in/DetailReceptionUseCase.java
@@ -2,16 +2,31 @@ package com.amenal.amenalbackend.achat.core.port.in;
 
 import java.util.List;
 
+import com.amenal.amenalbackend.achat.core.domain.DetailDevis;
 import com.amenal.amenalbackend.achat.core.domain.DetailReception;
+import com.amenal.amenalbackend.achat.core.domain.Reception;
+import com.amenal.amenalbackend.achat.core.port.out.DetailDevisDao;
 import com.amenal.amenalbackend.achat.core.port.out.DetailReceptionDao;
+import com.amenal.amenalbackend.achat.core.port.out.ReceptionDao;
+import com.amenal.amenalbackend.achat.infrastructure.adapter.out.postgres.entities.DetailDevisEntity;
+import com.amenal.amenalbackend.achat.infrastructure.adapter.out.postgres.entities.DetailReceptionEntity;
+import com.amenal.amenalbackend.achat.infrastructure.adapter.out.postgres.entities.ReceptionEntity;
+import com.amenal.amenalbackend.achat.infrastructure.dto.AddDetailReceptionDto;
+import com.amenal.amenalbackend.achat.infrastructure.dto.DetailDevisDto;
 import com.amenal.amenalbackend.achat.infrastructure.dto.DetailReceptionDto;
+import com.amenal.amenalbackend.achat.infrastructure.dto.ReceptionDto;
+import com.amenal.amenalbackend.utils.infrastructure.exception.DetailReceptionLargerThanDetailCommande;
 
 public class DetailReceptionUseCase {
 	
 	private DetailReceptionDao detailReceptionDao;
-	
-	public DetailReceptionUseCase(DetailReceptionDao detailReceptionDao) {
+
+	private DetailDevisDao detailDevisDao;
+
+
+	public DetailReceptionUseCase(DetailReceptionDao detailReceptionDao, DetailDevisDao detailDevisDao) {
 		this.detailReceptionDao = detailReceptionDao;
+		this.detailDevisDao = detailDevisDao;
 	}
 
 	public DetailReceptionDto findDetailReceptionById(Integer id) {
@@ -22,11 +37,43 @@ public class DetailReceptionUseCase {
 		return detailReceptionDao.findAllDetailReceptions();
 	}
 	
-	public DetailReception saveDetailReception(DetailReception detailReception) {
-		return detailReceptionDao.saveDetailReception(detailReception);
+	public DetailReception saveDetailReception(AddDetailReceptionDto addDetailReceptionDto) throws DetailReceptionLargerThanDetailCommande {
+
+		DetailDevisDto detailCommande = detailDevisDao.
+				findDetailDevisById(addDetailReceptionDto.getDetailCommandeId());
+		if (addDetailReceptionDto.getQte() != null && addDetailReceptionDto.getDetailCommandeId() != null){
+			Double qteReceptionne = addDetailReceptionDto.getQte();
+			Double qteCommande = detailCommande.getQte();
+			Double qteDejaReceptionne = detailReceptionDao
+					.findByDetailCommandeId(addDetailReceptionDto.getDetailCommandeId())
+					.stream()
+					.mapToDouble(DetailReceptionDto::getQte)
+					.sum();
+			if (qteReceptionne > qteCommande - qteDejaReceptionne){
+				throw new DetailReceptionLargerThanDetailCommande("La qte du articte receptionné est supérieur a son qantité commandé :"
+						+ qteCommande + " - son qantité déja recéptionné: "+ qteDejaReceptionne
+				);
+			}
+		}
+		return detailReceptionDao.saveDetailReception(addDetailReceptionDto);
 	}
 	
-	public DetailReception updateDetailReception(DetailReception detailReception) {
+	public DetailReception updateDetailReception(DetailReception detailReception) throws DetailReceptionLargerThanDetailCommande {
+		if (detailReception.getQte() != null && detailReception.getDetailCommande() != null
+				&& detailReception.getDetailCommande().getQte() != null){
+			Double qteReceptionne = detailReception.getQte();
+			Double qteCommande = detailReception.getDetailCommande().getQte();
+			Double qteDejaReceptionne = detailReceptionDao
+					.findByDetailCommandeId(detailReception.getDetailCommande().getId())
+					.stream()
+					.mapToDouble(DetailReceptionDto::getQte)
+					.sum();
+			if (qteReceptionne > qteCommande - qteDejaReceptionne){
+				throw new DetailReceptionLargerThanDetailCommande("La qte du articte receptionné est supérieur a son qantité commandé :"
+						+ qteCommande + " - son qantité déja recéptionné: "+ qteDejaReceptionne
+				);
+			}
+		}
 		return detailReceptionDao.updateDetailReception(detailReception);
 	}
 	
--- src/main/java/com/amenal/amenalbackend/achat/core/port/out/DetailReceptionDao.java
+++ src/main/java/com/amenal/amenalbackend/achat/core/port/out/DetailReceptionDao.java
@@ -3,17 +3,19 @@ package com.amenal.amenalbackend.achat.core.port.out;
 import java.util.List;
 
 import com.amenal.amenalbackend.achat.core.domain.DetailReception;
+import com.amenal.amenalbackend.achat.infrastructure.dto.AddDetailReceptionDto;
 import com.amenal.amenalbackend.achat.infrastructure.dto.DetailReceptionDto;
 
 public interface DetailReceptionDao {
 	DetailReceptionDto findDetailReceptionById(Integer id);
 	
 	List<DetailReceptionDto> findAllDetailReceptions();
-	
-	DetailReception saveDetailReception(DetailReception detailReception);
-	
-	DetailReception updateDetailReception(DetailReception detailReception);
+
+    DetailReception saveDetailReception(AddDetailReceptionDto detailReception);
+
+    DetailReception updateDetailReception(DetailReception detailReception);
 	
 	void deleteDetailReception(Integer id);
 
+    List<DetailReceptionDto> findByDetailCommandeId(Integer commandeId);
 }
